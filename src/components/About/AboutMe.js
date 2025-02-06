99c355b5f41d380f3c8621438c132862ec03c94d
Niharika Upadhyayula <niharika.u03@gmail.com>
2025-02-06 01:09:12 -0600

    song-update

--- src/components/About/AboutMe.js
+++ src/components/About/AboutMe.js
@@ -61,7 +61,7 @@ const displayRandomFact = () => {
                         {/* <p >Song of the Week:</p> */}
                         <iframe
                             style={{ borderRadius: '12px', }}
-                            src="https://open.spotify.com/embed/track/29WxJqIfDRMo9isV07kbJP?utm_source=generator"
+                            src="https://open.spotify.com/embed/track/5HaYRuf3zAfCb8RSO9LLea?utm_source=generator&theme=0"
                             width="100%"
                             height="140"
                             frameBorder="0"
